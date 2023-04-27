dd152784618f00bc7ed5d1740a6a2ee32618a38d
Marina <shalneva_mg@mail.ru>
2023-04-27 17:33:50 +0400

    Added program task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1. 
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+void PrintNum(int m)
+{
+    if(m >= 1){
+        Console.Write(m + " ");
+        PrintNum(m - 1);
+    }
+}
+
+int ReadInt(string argument)
+{
+    int number;
+    Console.Write($"Введите {argument}: ");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number!");
+    }
+    return number;
+}
+
+int numberA = ReadInt("значение N");
+PrintNum(numberA);
